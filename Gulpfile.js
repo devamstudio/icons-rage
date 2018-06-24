193526fdd813daa4f5d4027dd95b99572c657739
radiogron <kisasik1@gmail.com>
2018-06-25 01:19:05 +0400

    stable v.1.0.0

---
+++ Gulpfile.js
@@ -0,0 +1,62 @@
+const async = require('async');
+const gulp = require('gulp');
+const iconfont = require('gulp-iconfont');
+const consolidate = require('gulp-consolidate');
+const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const svgSprite = require("gulp-svg-sprites");
+ 
+gulp.task('iconfont', function(done){
+	var iconStream = gulp.src(['source/svg/*.svg'])
+		.pipe(iconfont({ 
+			fontName: 'rage-icons',
+			normalize: false,
+			prependUnicode: true,
+			startUnicode: 0xE001,
+			//fontHeight: 550,
+			formats: ['ttf', 'eot', 'woff','truetype','svg','woff2'],
+			copyright: 'iAmStudio',
+			centerHorizontally: false,
+			//ascent : 50,
+			descent : 60,
+		}));
+	async.parallel([
+		function handleGlyphs(cb) {
+			iconStream.on('glyphs', function(glyphs, options) {
+				// Font css
+				gulp.src('source/templates/font-rage.css')
+					.pipe(consolidate('lodash', {
+						glyphs: glyphs,
+						fontName: 'rage-icons',
+						fontPath: '../fonts/',
+						className: 'rage',
+					}))
+					.pipe(gulp.dest('dist/css/'))
+					.on('finish', cb);
+				console.log(glyphs, options);
+			});
+		},
+		function handleFonts(cb) {
+			iconStream
+				.pipe(gulp.dest('dist/fonts/'))
+				.on('finish', cb);
+		}
+	], done);
+});
+
+gulp.task('symbols', function () {
+	gulp.src('source/symbols/*.svg')
+		.pipe(svgSprite({
+			mode:'symbols',
+			svgId: 'rage-%f',
+			preview: false,
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('pug', function buildHTML() {
+	gulp.src('source/test/index.pug')
+		.pipe( plumber() )
+		.pipe( pug({ pretty: true }) )
+		.pipe( gulp.dest( 'test/' ) );
+});
